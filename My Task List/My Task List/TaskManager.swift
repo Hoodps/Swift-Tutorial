aa788c8fbe765911154e27843e99506fff90d3a5
Hoodps <791859814@qq.com>
2015-12-19 11:43:47 +0800

    add

---
+++ My Task List/My Task List/TaskManager.swift
@@ -0,0 +1,13 @@
+//
+//  TaskManager.swift
+//  My Task List
+//
+//  Created by xiexz on 15/12/19.
+//  Copyright © 2015年 xiexz. All rights reserved.
+//
+
+import Cocoa
+
+class TaskManager: NSObject {
+
+}
